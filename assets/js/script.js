269453f05ef74c6f49d8c1af0ecad69fb3851c6c
rody-1988 <xl.pogigori.808@gmail.com>
2026-08-03 12:38:39 +0900

    ページトップに戻る追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@
 const hamburger = document.querySelector('.c-hamburger');
 const nav = document.querySelector('.l-header__nav');
 const mask = document.querySelector('.c-mask');
+const pageTop = document.querySelector('.c-btn--top');
 
+//hamburgerメニュークリック時の処理
 hamburger.addEventListener('click',() => {
   hamburger.classList.toggle('is-active');
   nav.classList.toggle('is-active');
@@ -15,7 +17,17 @@ const header = document.querySelector('.l-header');
 window.addEventListener('scroll', () => {
   if (window.scrollY > window.innerHeight) {
     header.classList.add('is-scrolled');
+    pageTop.classList.add('is-active');
   } else {
     header.classList.remove('is-scrolled');
+    pageTop.classList.remove('is-active');
   }
+});
+
+//トップへ戻るボタンのクリック時の処理
+pageTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
 });
