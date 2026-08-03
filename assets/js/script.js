a92291c28e74a5f68d334bbbd7f880450bd9f2c4
rody-1988 <xl.pogigori.808@gmail.com>
2026-08-03 12:10:16 +0900

    header背景変わる

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,14 @@ hamburger.addEventListener('click',() => {
   nav.classList.toggle('is-active');
   mask.classList.toggle('is-active');
 });
+
+//スクロール時のヘッダー背景色変更
+const header = document.querySelector('.l-header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > window.innerHeight) {
+    header.classList.add('is-scrolled');
+  } else {
+    header.classList.remove('is-scrolled');
+  }
+});
